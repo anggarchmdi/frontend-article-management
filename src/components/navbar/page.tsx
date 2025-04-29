27dbb17741bd3e444af578cbd400acb2b7fa3363
anggarchmdi <anggarchmdi@gmail.com>
2025-04-29 14:30:59 +0700

    add navbar dummy

---
+++ src/components/navbar/page.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return(
+        <div className="w-screen fixed z-10 p-4  flex justify-between items-center bg-blue-500 px-12">
+            {/* Logo */}
+            <div className="">
+                <h1 className="font-semibold text-white text-xl">Lorem, ipsum.</h1>
+            </div>
+            {/* User Profile */}
+            <div className="text-white">lorem ipsum</div>
+        </div>
+    )
+}
