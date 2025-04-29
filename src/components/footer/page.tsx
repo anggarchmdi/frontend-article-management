9da6e362ed1307649d2411bee06bf10a51fea01a
anggarchmdi <anggarchmdi@gmail.com>
2025-04-29 14:29:14 +0700

    add footer dummy

---
+++ src/components/footer/page.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return(
+    <div className="w-screen flex justify-center items-center bg-[#2563EBDB]/86 text-white bottom-0 relative py-8 gap-x-4">
+        {/* logo */}
+        <h1 className="font-bold text-[1.3rem]">Loremipsum</h1>
+        <p className="text-[1rem]">©2025 Blog genzet. All rights reserved.</p>
+    </div>    
+    
+    )
+}
