72c0ec0ed04b88ea8892e2d88542da647b36110b
anggarchmdi <anggarchmdi@gmail.com>
2025-04-30 13:30:18 +0700

    fix articles | detail articles

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,7 +1,131 @@
+"use client"
+
+import { useParams } from "next/navigation"
+import { useEffect, useState } from "react"
+import axios from "@/lib/axios"
+import Navbar from "@/components/navbar/page"
+import Footer from "@/components/footer/page"
+import Link from "next/link"
+
 export default function DetailArticles() {
-    return(
-        <>
-        
-        </>
-    )
+  const { id } = useParams()
+  interface Article {
+    id: string
+    title: string
+    content: string
+    imageUrl: string
+    createdAt: string
+    user?: {
+      username?: string
+    }
+  }
+
+  const [article, setArticle] = useState<Article | null>(null)
+  interface RelatedArticle {
+    id: string
+    title: string
+    content: string
+    imageUrl: string
+    createdAt: string
+    category?: {
+      name?: string
+    }
+  }
+
+  const [relatedArticles, setRelatedArticles] = useState<RelatedArticle[]>([])
+
+  useEffect(() => {
+    if (!id) return
+
+    const fetchDetail = async () => {
+      try {
+        const res = await axios.get(`/articles/${id}`)
+        setArticle(res.data)
+      } catch (err) {
+        console.log("Gagal fetch detail article:", err)
+      }
+    }
+
+    const fetchRelated = async () => {
+      try {
+        const res = await axios.get("/articles", {
+          params: {
+            limit: 100,
+          },
+        })
+        const allArticles = res.data.data
+           // Filter out current article
+    const filtered = allArticles.filter((item: any) => item.id !== id)
+    // Shuffle & ambil 3
+    const shuffled = filtered.sort(() => 0.5 - Math.random()).slice(0, 3)
+    setRelatedArticles(shuffled)
+      } catch (err) {
+        console.log("Gagal fetch related articles:", err)
+      }
+    }
+
+    fetchDetail()
+    fetchRelated()
+  }, [id])
+
+  if (!article) return <div className="text-center p-8">Loading...</div>
+
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-4xl mx-auto px-4 py-8">
+        <div className="flex justify-center">
+
+        <p className="text-sm text-gray-500 mb-2">
+          {new Date(article.createdAt).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })} • Created by {article.user?.username || "Unknown"}
+        </p>
+        </div>
+        <h1 className="text-2xl lg:text-3xl font-bold mb-4 text-center">
+          {article.title}
+        </h1>
+        <img src={article.imageUrl || "/images/cardimg.jpg"} alt={article.title} className="w-full rounded-xl mb-6" />
+        <div className="prose max-w-none" dangerouslySetInnerHTML={{ __html: article.content }} />
+      </div>
+
+      <div className="max-w-6xl mx-auto px-4 py-12">
+        <h2 className="text-xl font-semibold mb-6">Other articles</h2>
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {relatedArticles.map((item) => (
+            <Link
+              key={item.id} href={`/articles/${item.id}`}>
+                <div className="bg-white rounded-lg min-h-[378px] shadow-md overflow-hidden hover:shadow-lg transition"
+            >
+              <img src={item.imageUrl || "/images/cardimg.jpg"} alt={item.title} className="w-full h-48 object-cover" />
+              <div className="p-4">
+                <p className="text-sm text-gray-500">
+                  {new Date(item.createdAt).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </p>
+                <h3 className="font-semibold text-lg mt-1 min-h-10 leading-6 line-clamp-2">
+                  {item.title}
+                </h3>
+                <p className="text-sm text-gray-600 mt-2 min-h-10 line-clamp-2">
+                  {item.content}
+                </p>
+                <div className="mt-3 flex flex-wrap gap-1">
+                  <span className="text-xs px-2 py-1 bg-blue-100 text-blue-700 rounded-full">
+                    {item.category?.name || "Uncategorized"}
+                  </span>
+                </div>
+              </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
 }
--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,9 +1,11 @@
 "use client"
+import Link from 'next/link'
 import Navbar from "@/components/navbar/page"
 import Image from "next/image"
 import axios from "@/lib/axios"
 import { useEffect, useState } from "react"
 import Footer from "@/components/footer/page"
+
 import {
     Pagination,
     PaginationContent,
@@ -12,7 +14,7 @@ import {
     PaginationNext,
     PaginationPrevious,
   } from "@/components/ui/pagination";
-import { title } from "process"
+// import { title } from "process"
 
   export default function Articles() {
     const [articles, setArticles] = useState<any[]>([]);
@@ -21,7 +23,7 @@ import { title } from "process"
     const [search, setSearch] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
-    const itemsPerPage = 9;
+    const [itemsPerPage, setItemPerPage] = useState(9)
 
     const fetchArticles = async () => {
             try{
@@ -41,10 +43,20 @@ import { title } from "process"
                 console.log("Gagal fetch Articles: ", err)
             }
         }
+        
+        useEffect(() => {
+            const handleResize = () => {
+              const width = window.innerWidth;
+              setItemPerPage(width <= 1080 ? 3 : 9);
+            };
+            handleResize();
+            window.addEventListener("resize", handleResize);
+            return () => window.removeEventListener("resize", handleResize);
+          }, []);
 
         useEffect(() => {
             fetchArticles()
-        }, [currentPage, SelectedCategory, search])
+        }, [currentPage, SelectedCategory, search, itemsPerPage])
 
         useEffect(() => {
             const fetchCategory = async () => {
@@ -67,40 +79,40 @@ import { title } from "process"
         return(
         <>
         <Navbar />
-        <div className="w-screen bg-gray-100">
+        <div className="w-full h-full bg-gray-100">
             {/* hero section */}
             <div className="h-[500px] relative w-full">
             {/* img */}
             <img src="/images/hero-section.jpg" alt="hero" className="object-cover h-[500px] w-full -z-10" />
             {/* overlay */}
             <div className="absolute flex justify-center items-center inset-0 bg-[#2563EB]/86">
-            <div className="flex flex-col text-center justify-center items-center text-white gap-y-[12px]">
-                <h1 className="font-bold text-[1rem]">Blog Genzet</h1>
-                <h1 className="text-[3rem] font-medium w-[720px] leading-14">The Journal : Design Resources, Interviews, Industry News</h1>
-                <h1 className="text-[1.5rem] ">Your daily close of design insight!</h1>
-                {/* search */}
-                <div className="w-[608px] h-[60px] bg-[#3B82F6] p-[10px] rounded-[12px] mt-[40px] flex gap-x-4 overflow-hidden">
+            <div className="flex flex-wrap flex-col text-center justify-center items-center text-white gap-y-[12px]">
+                    <h1 className="font-bold text-[0.875rem] lg:text-[1rem]">Blog Genzet</h1>
+                    <h1 className="text-[2.125rem] lg:text-[3rem] font-medium lg:w-[720px] leading-14">The Journal : Design Resources, Interviews, Industry News</h1>
+                    <h1 className="text-[1.25rem] lg:text-[1.5rem] ">Your daily close of design insight!</h1>
+                {/* search & dropdown */}
+                <div className="w-[337px] lg:w-[608px] h-[108px] lg:h-[60px] bg-[#3B82F6] p-[10px] rounded-[12px] mt-[40px] flex flex-col lg:flex-row gap-x-2 gap-y-[8px] overflow-hidden">
                 {/* dropdown */}
-                <div className="w-[40%] bg-white text-gray-600 rounded-[12px]">
-                <select
-                    className="w-full h-full px-4 py-2 rounded-[12px] outline-none"
-                    value={SelectedCategory}
-                    onChange={(e) => {
-                        setSelectedCategory(e.target.value)
-                        setCurrentPage(1)
-                    }}
-                >
-                    <option value="">All Categories</option>
-                    {categories.map((cat) => (
-                        <option key={cat.id} value={cat.id}>
-                            {cat.name}
-                        </option>
-                    ))}
-                </select>
+                <div className="w-full lg:w-[40%] h-full bg-white text-gray-600 rounded-[12px]">
+                    <select
+                        className="w-full h-full px-4 py-2 rounded-[12px] outline-none"
+                        value={SelectedCategory}
+                        onChange={(e) => {
+                            setSelectedCategory(e.target.value)
+                            setCurrentPage(1)
+                        }}
+                    >
+                        <option value="">All Categories</option>
+                        {categories.map((cat) => (
+                            <option key={cat.id} value={cat.id}>
+                                {cat.name}
+                            </option>
+                        ))}
+                    </select>
                 </div>
 
                 {/* search */}
-                <div className="w-[60%] bg-white rounded-[12px] flex">
+                <div className="w-full lg:w-[60%] h-full bg-white rounded-[12px] flex">
                 <input 
                 type="text"
                 className="w-full rounded-[12px] text-gray-700 px-4"
@@ -111,40 +123,50 @@ import { title } from "process"
                     setCurrentPage(1)
                 }}
                 />
-                    </div>
-                        </div>
+                </div>
+            </div>
                     </div>
                 </div>
             </div>
             {/* content */}
-            <div className="h-[1728px]">
-                <div className="mx-auto container p-12">
+            <div className="max-h-[1780px] lg:max-h-[1680px] w-full">
+                <div className="mx-auto container p-6 lg:p-12">
                     <p className="mb-6 text-[1rem] font-semibold text-gray-500">Lorem ipsum dolor sit.</p>
-                    <div className="grid grid-cols-3 space-y-12">
+                    <div className="grid md:grid-cols-2 lg:grid-cols-3 space-y-12">
                         {/* card */}
-                        {articles.map((item, index) => (
-                            <div key={index} className="transition-all transform hover:scale-105 hover:cursor-pointer  duration-300 w-[400px] h-[432px] rounded-t-xl  space-y-2">
+                        {articles.map((item) => (
+                            <Link key={item.id} href={`/articles/${item.id}`}>  
+                            <div className="transition-all transform hover:scale-95 hover:cursor-pointer  duration-300 w-[335px] h-[368px] lg:w-[400px] lg:h-[432px] rounded-t-xl  space-y-2">
                             {/* img */}
                             <div className="h-[240px] flex justify-center">
-                                <img src={item.imageUrl} className="w-full rounded-xl object-cover" alt="" />
+                                <img src={item.imageUrl || "/images/cardimg.jpg"} className="w-full rounded-xl object-cover" alt="" />
                             </div>
-                            {/* time */}
-                            <p className="text-[#475569]">{item.createdAt}</p>
+                           {/* time */}
+                            <p className="text-[#475569]">
+                            {new Date(item.createdAt).toLocaleDateString("en-US", {
+                                year: "numeric",
+                                month: "long",
+                                day: "numeric",
+                            })}
+                            </p>
                             {/* title */}
-                            <h1 className="text-lg font-semibold line-clamp-2">{item.title}</h1>
+                            <h1 className="text-lg min-h-5 font-semibold line-clamp-1">{item.title}</h1>
                             {/* desc */}
-                            <p className="line-clamp-2">{item.content}</p>
-                            {/* category */}
-                            {/* <div className="flex gap-2 flex-wrap">
-                                {item.category.map((cat, idx) => (
-                                    <h1 key={idx} className="px-4 py-1.5 text-[0.8rem] rounded-2xl bg-[#BFDBFE] text-[#1E3A8A] gap-2">{cat}</h1>
-                                ))}
-                            </div> */}
-                        </div>
-                    ))}
+                            <p className="line-clamp-2 min-h-10">{item.content}</p>
+                          {/* category */}
+                            {item.category && (
+                            <div className="flex gap-2 flex-wrap">
+                                <span className="px-4 py-1.5 text-[0.8rem] rounded-2xl bg-[#BFDBFE] text-[#1E3A8A]">
+                                {item.category.name}
+                                </span>
+                            </div>
+                            )}
+                            </div>
+                            </Link>
+                         ))}
                 </div>
                 {/* paginate */}
-                <Pagination className="mt-8">
+                <Pagination className="lg:mt-8 mt-24">
                     <PaginationContent>
                         <PaginationItem>
                         <PaginationPrevious
